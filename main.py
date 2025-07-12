8ab6c955ce6c634725653658290c6102f9853004
P1X3R <p1x3r@proton.me>
2025-07-12 10:28:37 -0500

    Value and policy heads

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 BOARD_LENGTH = 8
 BOARD_VECTOR_DEPTH = 12
+POLICY_OUTPUT_SIZE = 4672  # You can change this based on move encoding
 
 
-# Double head architecture
 class Alpha(nn.Module):
     def __init__(self):
         super(Alpha, self).__init__()
@@ -22,32 +22,40 @@ class Alpha(nn.Module):
         self.bn3 = nn.BatchNorm2d(128)
         self.bn4 = nn.BatchNorm2d(256)
 
-        self.fc1 = nn.Linear(256 * 8 * 8, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-
-        self.dr1 = nn.Dropout()
-        self.dr2 = nn.Dropout()
-
         # Policy head
+        self.policy_conv = nn.Conv2d(256, 2, kernel_size=1)
+        self.policy_bn = nn.BatchNorm2d(2)
+        self.policy_fc = nn.Linear(2 * 8 * 8, POLICY_OUTPUT_SIZE)
+        self.policy_dr = nn.Dropout()
 
         # Value head
+        self.value_conv = nn.Conv2d(256, 1, kernel_size=1)
+        self.value_bn = nn.BatchNorm2d(1)
+        self.value_fc1 = nn.Linear(1 * 8 * 8, 256)
+        self.value_fc2 = nn.Linear(256, 1)
+        self.value_dr1 = nn.Dropout()
+        self.value_dr2 = nn.Dropout()
 
     def forward(self, x):
-        # x is assumed to be (batch_size, BOARD_VECTOR_DEPTH, BOARD_LENGTH, BOADR_LENGTH)
-
-        # Common body
+        # Shared body
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.bn4(self.conv4(x)))
-
-        x = x.view(-1, 256 * 8 * 8)
-        x = F.relu(self.dr1(self.fc1(x)))
-        x = F.relu(self.dr2(self.fc2(x)))
+        x_conv = F.relu(self.bn4(self.conv4(x)))  # Save for heads
 
         # Policy head
+        x_policy = F.relu(self.policy_bn(self.policy_conv(x_conv)))
+        x_policy = x_policy.view(-1, 2 * 8 * 8)
+        x_policy = self.policy_fc(self.policy_dr(x_policy))
+        x_policy = F.log_softmax(x_policy, dim=1)
 
         # Value head
+        x_value = F.relu(self.value_bn(self.value_conv(x_conv)))
+        x_value = x_value.view(-1, 8 * 8)
+        x_value = F.relu(self.value_fc1(self.value_dr1(x_value)))
+        x_value = torch.tanh(self.value_fc2(self.value_dr2(x_value)))
+
+        return x_policy, x_value
 
 
 def main():
