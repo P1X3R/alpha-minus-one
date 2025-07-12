f71dd0599cb627458da8922344852debe781143d
P1X3R <p1x3r@proton.me>
2025-07-12 15:13:10 -0500

    Make a chess engine play against itself to create the custom dataset

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import chess
+import chess.engine
+import chess.polyglot
+import asyncio
+import random
 
 BOARD_LENGTH = 8
 BOARD_VECTOR_DEPTH = 12
@@ -35,7 +40,7 @@ class Alpha(nn.Module):
         self.value_dr1 = nn.Dropout()
         self.value_dr2 = nn.Dropout()
 
-    def forward(self, x):
+    def forward(self, x) -> tuple[torch.Tensor, torch.Tensor]:
         # Shared convolutional body
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
@@ -60,11 +65,52 @@ class Alpha(nn.Module):
         return x_policy, x_value
 
 
-alpha_minus_one = Alpha()
+# Makes a chess engine play against itself, storing position (in FEN) and best move (in UCI algebraic notation)
+async def collect_game_data(
+    engine_executable: str = "stockfish",
+    opening_book_path: str = "book.bin",
+    max_opening_moves: int = 12,
+) -> list[tuple[str, str]]:
+    data = []
+    transport, engine = await chess.engine.popen_uci(engine_executable)
+
+    board = chess.Board()
+    move_count = 0
+
+    with chess.polyglot.open_reader(opening_book_path) as book:
+        while not board.is_game_over():
+            if move_count < max_opening_moves:
+                try:
+                    entry = random.choice(list(book.find_all(board)))
+                    move = entry.move
+                    print(
+                        f"Game {len(data) // 2 + 1}, Move {move_count + 1}: {board.san(move)} (from book)"
+                    )
+                    data.append((board.fen(), move.uci()))
+                    board.push(move)
+                except IndexError:
+                    result = await engine.play(board, chess.engine.Limit(time=0.1))
+                    print(
+                        f"Game {len(data) // 2 + 1}, Move {move_count + 1}: {board.san(result.move)} (from Stockfish)"
+                    )
+                    data.append((board.fen(), result.move.uci()))
+                    board.push(result.move)
+            else:
+                result = await engine.play(board, chess.engine.Limit(time=0.1))
+                print(
+                    f"Game {len(data) // 2 + 1}, Move {move_count + 1}: {board.san(result.move)} (from Stockfish)"
+                )
+                data.append((board.fen(), result.move.uci()))
+                board.push(result.move)
+            move_count += 1
+
+    await engine.quit()
+    return data
 
 
-def main():
-    print("Hello from alpha-1!")
+def main() -> None:
+    print("Playing game:")
+    _ = asyncio.run(collect_game_data())
 
 
 if __name__ == "__main__":
