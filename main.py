a0d0d00fc64695bd5e1d6f57d00e5f51ae8a6c45
P1X3R <p1x3r@proton.me>
2025-07-12 11:22:56 -0500

    Refine policy head and add comments

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 
 BOARD_LENGTH = 8
 BOARD_VECTOR_DEPTH = 12
-POLICY_OUTPUT_SIZE = 4672  # You can change this based on move encoding
+POLICY_OUTPUT_SIZE = 4672  # 8x8 board × 73 possible moves per square
 
 
 class Alpha(nn.Module):
     def __init__(self):
         super(Alpha, self).__init__()
 
-        # Shared body
+        # Shared convolutional body
         self.conv1 = nn.Conv2d(BOARD_VECTOR_DEPTH, 32, kernel_size=5, padding=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
@@ -22,13 +22,11 @@ class Alpha(nn.Module):
         self.bn3 = nn.BatchNorm2d(128)
         self.bn4 = nn.BatchNorm2d(256)
 
-        # Policy head
-        self.policy_conv = nn.Conv2d(256, 2, kernel_size=1)
-        self.policy_bn = nn.BatchNorm2d(2)
-        self.policy_fc = nn.Linear(2 * 8 * 8, POLICY_OUTPUT_SIZE)
-        self.policy_dr = nn.Dropout()
+        # Policy head: predicts a 73-channel move distribution for each square
+        self.policy_conv = nn.Conv2d(256, 73, kernel_size=1)
+        self.policy_bn = nn.BatchNorm2d(73)
 
-        # Value head
+        # Value head: evaluates board position as a scalar in [-1, 1]
         self.value_conv = nn.Conv2d(256, 1, kernel_size=1)
         self.value_bn = nn.BatchNorm2d(1)
         self.value_fc1 = nn.Linear(1 * 8 * 8, 256)
@@ -37,23 +35,24 @@ class Alpha(nn.Module):
         self.value_dr2 = nn.Dropout()
 
     def forward(self, x):
-        # Shared body
+        # Shared convolutional body
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x_conv = F.relu(self.bn4(self.conv4(x)))  # Save for heads
+        x_conv = F.relu(self.bn4(self.conv4(x)))  # Shared features
 
-        # Policy head
+        # Policy head forward pass
         x_policy = F.relu(self.policy_bn(self.policy_conv(x_conv)))
-        x_policy = x_policy.view(-1, 2 * 8 * 8)
-        x_policy = self.policy_fc(self.policy_dr(x_policy))
-        x_policy = F.log_softmax(x_policy, dim=1)
+        x_policy = x_policy.view(-1, 73 * 8 * 8)  # Flatten to [batch_size, 4672]
+        x_policy = F.log_softmax(x_policy, dim=1)  # Log-probabilities for NLLLoss
 
-        # Value head
+        # Value head forward pass
         x_value = F.relu(self.value_bn(self.value_conv(x_conv)))
-        x_value = x_value.view(-1, 8 * 8)
+        x_value = x_value.view(-1, 8 * 8)  # Flatten spatial dimensions
         x_value = F.relu(self.value_fc1(self.value_dr1(x_value)))
-        x_value = torch.tanh(self.value_fc2(self.value_dr2(x_value)))
+        x_value = torch.tanh(
+            self.value_fc2(self.value_dr2(x_value))
+        )  # Output ∈ [-1, 1]
 
         return x_policy, x_value
 
